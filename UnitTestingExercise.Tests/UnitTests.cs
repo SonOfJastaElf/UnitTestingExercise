c3c604e5505284c2326c8492a7e0f4c4d3ef3dff
Brian Henderson <henderson.brian82@gmail.com>
2020-09-14 15:40:34 -0400

    Exercise is all done

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,86 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData (5,5,5,15)]
+        [InlineData(6,7,8,21)]
+        [InlineData(0,3,5,8)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var testOne = new UnitTestMethods();
             //Act
-
+            var actual = testOne.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(7,3,4)]
+        [InlineData(6,5,1)]
+        [InlineData(7,1,6)]
+        [InlineData(7,7,0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var testTwo = new UnitTestMethods();
             //Act
-
+            var actual = testTwo.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(8,2,16)]
+        [InlineData(4,7,28)]
+        [InlineData(6,7,42)]
+        [InlineData(0,9,0)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testThree = new UnitTestMethods();
             //Act
-
+            var actual = testThree.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(9,3,3)]
+        [InlineData(15,3,5)]
+        [InlineData(18,2,9)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testFour = new UnitTestMethods();
             //Act
-
+            var actual = testFour.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
             //Arrange
-
+            var expected = "Oh, hi!";
+            var instanceOne = new UnitTestMethods();
             //Act
-
+            var actual = instanceOne.ActualOne();
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName2()
         {
             //Arrange
-
+            var expected = '@';
+            var instanceTwo = new UnitTestMethods();
             //Act
-
+            var actual = instanceTwo.ActualTwo();
             //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,25 +1,45 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
 
-
-
-        // Create an Add method that passes 3 integers
-
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
+        
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
-
+        public int Subtract (int one, int two)
+        {
+            return one - two;
+        }
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int one, int two)
+        {
+            return one * two;
+        }
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int one, int two)
+        {
+            return one / two;
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string ActualOne()
+        {
+            
+            return "Oh, hi!";
+        }
+
+        public char ActualTwo()
+        {
+            return '@';
+        }
 
         
     }
